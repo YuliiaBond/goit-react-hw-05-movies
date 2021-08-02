5cfc056d4e8dc8a0d92c58cba6776d8de4bafb79
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-08-02 18:52:14 +0300

    +

---
+++ src/services/api.js
@@ -0,0 +1,2 @@
+// const BASE_URL = 'https://api.themoviedb.org/3';
+// const KEY = '08685f82d21c93cd92857dcadddfeb71';
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-const HomePage = () => {
-    return <h1>домашняя страница со списком популярных кинофильмов</h1>
+class HomePage extends Component {
+    state = {
+        movies: [],
+    };
+
+    async componentDidMount() {
+        const response = await axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=08685f82d21c93cd92857dcadddfeb71&language=en-US')
+        console.log(response.data);
+        console.log(response.data.results);
+        this.setState({ movies: response.data.results });
+    }
+    render() {
+        return (
+            <>
+                <h1>домашняя страница со списком популярных кинофильмов</h1>
+                
+                <ul>
+                    {this.state.movies.map(movie => (
+                        <li key={movie.id}>{movie.title}</li>
+                    ))}
+                </ul>
+            </>
+        )
+
+    }
+    
 };
 
 export default HomePage;
