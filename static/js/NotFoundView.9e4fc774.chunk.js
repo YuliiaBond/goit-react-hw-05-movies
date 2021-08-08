f6447d636a6ac9e510e1d29c93e86c34165629a4
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-08-09 01:09:18 +0300

    Updates

---
+++ static/js/NotFoundView.9e4fc774.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[4],{62:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return o}));var i=n(1);function o(){return Object(i.jsx)("h1",{children:"404 - page not found"})}}}]);
+//# sourceMappingURL=NotFoundView.9e4fc774.chunk.js.map
